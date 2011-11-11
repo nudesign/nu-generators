7f85f75aa8e76ca18b6bfc03e2a06b15b223a7a7
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2011-11-11 16:42:15 -0200

    changing the scaffold generator to create view files

--- lib/generators/nu/scaffold_generator.rb
+++ lib/generators/nu/scaffold_generator.rb
@@ -3,10 +3,20 @@ module Nu
     class ScaffoldGenerator < Rails::Generators::NamedBase
       source_root File.expand_path('../templates', __FILE__)
       
+      argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
+      
       hook_for :orm, :as => :model
       
       def create_controller_file
         template 'controller.rb', "app/controllers/#{plural_name}_controller.rb"
+      end
+      
+      def create_views_files
+        views = %w(all edit show new _form)
+        
+        views.each do |view|
+          template "views/#{view}.html.erb", "app/views/#{plural_name}/#{view}.html.erb"
+        end
       end
     end
   end
